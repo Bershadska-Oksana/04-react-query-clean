3de73af93e96d9b9b66e3f301be90b2d7ca4d562
Oksana Bershadska <ksenaua@icloud.com>
2025-10-24 21:06:11 +0200

    initial setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Test: 04-react-query clean</h1>
+      <p>If you see this, app runs locally.</p>
+    </div>
+  );
+};
+
+export default App;
